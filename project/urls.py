2f919eba1ff66caa621fb1c7c8c7c8b967ec30b7
Ahmed Magdy <94301115+AhmedDR200@users.noreply.github.com>
2023-08-05 00:11:58 +0300

    API setup , DONE!!

--- project/urls.py
+++ project/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path , include
+from api.models import ModelResource
+
+movie_resource = ModelResource()
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('movies/' ,include('movies.urls') ),
+    path('movies/' ,include('movies.urls')),
+    path('api/' ,include(movie_resource.urls)), #api
 ]
