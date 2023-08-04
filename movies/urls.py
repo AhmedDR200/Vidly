6d581fb2059bc2efa2b4ea1e90d40f4e04e89ce9
Ahmed Magdy <94301115+AhmedDR200@users.noreply.github.com>
2023-08-04 23:40:24 +0300

    templates done

--- movies/urls.py
+++ movies/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('' , views.index, name='index')
+    path('' , views.index, name='movies_index'),
+    path('<int:movie_id>', views.detail, name='datail')
 ]
--- movies/views.py
+++ movies/views.py
@@ -9,3 +9,9 @@ def index(request):
     # select * from movies_movie
     movies = Movie.objects.all()
     return render(request, 'movies/index.html', {'movies':movies})
+
+
+
+def detail(request, movie_id):
+    movie = Movie.objects.get(pk=movie_id)
+    return render(request, 'movies/detail.html', {'movie':movie})
